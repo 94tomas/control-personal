74b1682730fe176629e50741d091c5b76c8ba15d
94tomas <nos18de@gmail.com>
2021-08-09 13:09:50 -0400

    first commit

--- public/js/app.js
+++ public/js/app.js
@@ -50006,8 +50006,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\personal-control\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\personal-control\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/edson/Projects/personal-control/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/edson/Projects/personal-control/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
